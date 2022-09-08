e3c0c1c25ea34b26ace88a3d75e6157c7c9b1261
IrynaTereshko <i.tereshko@list.ru>
2022-09-08 11:26:51 +0300

    new version

--- src/Task06.java
+++ src/Task06.java
@@ -5,19 +5,21 @@ public class Task06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-
+        int quantity;
+do {
         System.out.println("Enter quantity of random repeats: ");
-        int quantity = scanner.nextInt();
+        quantity = scanner.nextInt();
+}while(quantity<=0);
 
-        int positive = 0, negative = 0;
+        int positive = 0;
 
         for (int i = 0; i < quantity; i++) {
             if (random.nextBoolean()) {
-                positive += 1;
-            } else {
-                negative += 1;
+                positive ++;
             }
+
         }
-        System.out.println("Quantity of true is " + positive + ", quantity of false is " + negative + ".");
+        String msg = String.format ("Quantity of true is" d%, "quantity of false is" d% ": positive, quantity-positive);
+        System.out.print(msg);
     }
 }
