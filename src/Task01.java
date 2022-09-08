7a8f6e8e38d354b0f87f4ad91fa6619df2a5322e
IrinaTereshko <i.tereshko@list.ru>
2022-09-08 09:32:48 +0300

    updated task 01

--- src/Task01.java
+++ src/Task01.java
@@ -3,5 +3,9 @@ import java.util.Arrays;
 public class Task01 {
     public static void main(String[] args) {
        System.out.print("I'm testing GitHub");
+        System.out.print("I'm testing GitHub");
+        System.out.print("I'm testing GitHub");
+        System.out.print("I'm testing GitHub");
+        System.out.print("I'm testing GitHub");
     }
 }
