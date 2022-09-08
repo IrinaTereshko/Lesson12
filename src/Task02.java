87652279f102377d49f32e09a66814cd233a1214
IrynaTereshko <i.tereshko@list.ru>
2022-09-08 10:16:01 +0300

    random from 10 to 20

--- src/Task02.java
+++ src/Task02.java
@@ -3,10 +3,11 @@ import java.util.Random;
 public class Task02 {
     public static void main(String[] args) {
         Random random = new Random();
-        // random integer number  from 0 to 100
-        int bound = 100;
+        // random integer number  from 10 to 20, if +1 then 20 included in random
+        int b = 20;
+        int a = 10;
         for (int i = 0; i < 10; i++) {
-            System.out.println(random.nextInt(bound));
+            System.out.println(random.nextInt(b-a+1)+a );
         } {
 
         }
