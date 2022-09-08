205f85f66d84e322d3a867f79aea0875e1b2b187
IrynaTereshko <i.tereshko@list.ru>
2022-09-08 09:56:23 +0300

    using static Math.random

--- src/Task01.java
+++ src/Task01.java
@@ -2,10 +2,12 @@ import java.util.Arrays;
 
 public class Task01 {
     public static void main(String[] args) {
-       System.out.print("I'm testing GitHub");
-        System.out.print("I'm testing GitHub");
-        System.out.print("I'm testing GitHub");
-        System.out.print("I'm testing GitHub");
-        System.out.print("I'm testing GitHub");
+       System.out.println ((int) (Math.random()*100));
+        System.out.println ((int) (Math.random()*100));
+        System.out.println ((int) (Math.random()*100));
+        System.out.println ((int) (Math.random()*100));
+        System.out.println ((int) (Math.random()*100));
+
+
     }
 }
